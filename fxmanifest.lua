bae32854223ba2d3f710dd03e4171562e2b892f4
Rico® <177413802+realricos@users.noreply.github.com>
2025-03-23 01:19:01 +0100

    Uploaded fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Rico'
+lua54 'yes'
+description 'Speedcameras'
+version '1.0.0'
+
+client_scripts {
+    'backend/client/client.lua',
+    'backend/shared/config.lua'
+}
+server_scripts {
+    'backend/server/server.lua'
+}
